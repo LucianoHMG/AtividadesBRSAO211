a12846ca4130c9203d21416f24d53023aa3dccf0
LucianoHMG <Lucianowtp@gmail.com>
2026-01-08 20:32:36 -0300

    ativade 01 questao3

---
+++ AtividadePratica01/questao3.py
@@ -0,0 +1,12 @@
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print("O volume da caixa é:", volume, "cm³") 
+# Adicionei comentários para explicar o código
+# Calcula o volume de uma caixa retangular  
+
+
+
+
+
