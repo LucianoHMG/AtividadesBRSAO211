23bbea43018d6b15deae0c4a789301406d96d9bd
LucianoHMG <Lucianowtp@gmail.com>
2026-01-12 22:07:01 -0300

    ativade 3 questao 4

---
+++ AtividadePratica03/questao4.py
@@ -0,0 +1,8 @@
+print("=== VERIFICADOR DE ANO BISSEXTO ===")
+
+ano = int(input("Digite um ano: "))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"{ano} é um ano bissexto.")
+else:
+    print(f"{ano} NÃO é um ano bissexto.")
